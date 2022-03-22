df78a179a29f759819fe144389cd33115482511b
Vassilis Kotsmanidis <vassilis.kotsman@gmail.com>
2022-03-22 20:42:40 +0200

    Initial folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './modules/admin/admin.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAYREN_M-TONfv2iH_b3M7EGLqQ-PETyhg",
+    authDomain: "game-over-webapp.firebaseapp.com",
+    projectId: "game-over-webapp",
+    storageBucket: "game-over-webapp.appspot.com",
+    messagingSenderId: "597503331914",
+    appId: "1:597503331914:web:22570fd6547142bb18e0b3"
+  }
 };
 
 /*
